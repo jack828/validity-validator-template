b69a4de7a35632fae766bd3bc7a5a8858c1f71f2
Jack Burgess <jackburgess124@gmail.com>
2020-02-01 13:20:14 +0000

    setup tests

---
+++ index.js
@@ -0,0 +1,5 @@
+const validityValidator = (key, keyDisplayName, object, cb) => {
+  cb(null, 'You need to modify the template to use this validator')
+}
+
+module.exports = validityValidator
---
+++ test/validator.test.js
@@ -0,0 +1,7 @@
+// const assert = require('assert')
+// const validator = require('../')
+
+describe('Validator', () => {
+  it('should validate valid data')
+  it('should validate invalid data')
+})
